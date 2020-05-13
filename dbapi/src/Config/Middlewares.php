b343a4b9b7280c7ccde5e32bb025fcf9c0dfc2f9
mahdyaslami <mahdyaslami@gmail.com>
2020-05-13 15:26:39 +0430

    remove unused package

--- dbapi/src/Config/Middlewares.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Middleware\JsonBodyParserMiddleware;
-
-/**
- * Add middleware for manage body when content type is 'application/json'.
- */
-$app->add(new JsonBodyParserMiddleware());
