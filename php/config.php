178e49aed2aec94d8ddd38d15c27b96c7e8e9090
mahdyaslami <mahdyaslami@gmail.com>
2020-05-13 11:21:34 +0430

    remove old files

--- php/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/**
- * version of app.
- */
-define('VERSION', '1.1.4');
--- php/data/save.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (isset($_POST['filename']) && file_exists(__DIR__ . '/../../data/' . $_POST['filename'])) {
-    echo file_put_contents(__DIR__ . '/../../data/' . $_POST['filename'], $_POST['data']);
-} else {
-    echo 0;
-}
