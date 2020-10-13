89606d69257264674d3909831b4322254613c538
unknown <Mahdi.Aslami@PART.LOCAL>
2020-10-13 17:31:13 +0330

    Add account.

---
+++ database/migrations/2020_10_13_135756_create_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('domain', 255);
+            $table->string('username', 255);
+            $table->string('passwords', 255);
+            $table->string('description', 4000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
