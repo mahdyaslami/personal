575abbb09da00fb396c7e48aae98b448e5c80658
unknown <Mahdi.Aslami@PART.LOCAL>
2020-10-13 17:35:28 +0330

    Get all accounts.

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
-    //
+    public function index()
+    {
+        return Account::all();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AccountController;
+use App\Models\Account;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+$router->get('/accounts', [AccountController::class, 'index']);
