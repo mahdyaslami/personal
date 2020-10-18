29ffdc4de0ffeecbcd13851f6e7ffa4eb6d241f8
unknown <Mahdi.Aslami@PART.LOCAL>
2020-10-18 17:12:56 +0330

    typo.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -15,7 +15,7 @@
         @yield("content")
 
         <footer class="my-5 pt-5 text-muted text-center text-small">
-            <p class="mb-1">&copy; 2017-2020 Mahdi Aslami Khavar</p>
+            <p class="mb-1">&copy; 2017-2020 Mahdi Aslami Khavari</p>
             <ul class="list-inline">
                 <li class="list-inline-item"><a href="#">Privacy</a></li>
                 <li class="list-inline-item"><a href="#">Terms</a></li>
