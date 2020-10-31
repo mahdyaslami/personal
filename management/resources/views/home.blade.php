95d3dd3c6e684bc4651de94bb5457721a3ea9bee
mahdyaslami <mahdyaslami@gmail.com>
2020-10-31 12:31:27 +0330

    Update home.

--- management/resources/views/home.blade.php
+++ management/resources/views/home.blade.php
@@ -4,7 +4,15 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card mb-2">
+                <div class="card-header">{{ __('Accounts') }}</div>
+
+                <div class="card-body">
+                    <a href="{{ route('accounts.index') }}">Accounts</a>
+                </div>
+            </div>
+
+            <div class="card mb-2">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
