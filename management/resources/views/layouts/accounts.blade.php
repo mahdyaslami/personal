a2f350ecf491de78b0c6c8d9a31fdbc83c6c1963
mahdyaslami <mahdyaslami@gmail.com>
2020-11-07 11:47:07 +0330

    Add crypto js.

--- management/resources/views/layouts/accounts.blade.php
+++ management/resources/views/layouts/accounts.blade.php
@@ -17,3 +17,7 @@
 </div>
 
 @endsection
+
+@section('script')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js" integrity="sha512-nOQuvD9nKirvxDdvQ9OMqe2dgapbPB7vYAMrzJihw5m+aNcf0dX53m6YxM4LgA9u8e9eg9QX+/+mPu8kCNpV2A==" crossorigin="anonymous"></script>
+@endsection
