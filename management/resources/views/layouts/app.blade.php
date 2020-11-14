bc4a385cc7a1a2e4ef1da6b24dce8964afe4d461
mahdyaslami <mahdyaslami@gmail.com>
2020-11-14 17:04:53 +0330

    Use turbo link.

--- management/resources/views/layouts/app.blade.php
+++ management/resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
-                <a class="navbar-brand font-weight-bolder" href="{{ url('/') }}">
+                <a class="navbar-brand font-weight-bolder" href="{{ url('/') }}" data-turbolinks="false">
                     Mahdi Aslami Khavari
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
